26a6b3f99da8c1a0874444229b5c92a2c75edf65
rashadaghayev85 <rashadra@code.edu.az>
2024-06-25 02:15:57 +0400

    Completed task

---
+++ API-Architecture/API-Architecture/Controllers/Admin/CountryController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.DTOs.Admin.Countries;
+using Service.Services.Interfaces;
+
+namespace API_Architecture.Controllers.Admin
+{
+    public class CountryController : BaseController
+    {
+        private readonly ICountryService _countryservice;
+
+        public CountryController(ICountryService countryService)
+        {
+            _countryservice = countryService;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return Ok(await _countryservice.GetAll());
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create([FromBody] CountryCreateDto request)
+        {
+            try
+            {
+                await _countryservice.CreateAsync(request);
+
+                return CreatedAtAction(nameof(Create), new { response = "Data successfully created" });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            try
+            {
+                return Ok(await _countryservice.GetByIdAsync(id));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] int? id)
+        {
+            if (id == null) return BadRequest();
+            await _countryservice.DeleteAsync((int)id);
+
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit([FromRoute]int id,[FromBody] CountryEditDto request)
+        {
+            await _countryservice.EditAsync(id, request);
+            return Ok();
+        }
+    }
+}
---
+++ API-Architecture/Service/Helpers/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Service.DTOs.Admin.Countries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Country, CountryDto>();
+            CreateMap<CountryCreateDto, Country>();
+            CreateMap<CountryEditDto, Country>();
+        }
+    }
+}
